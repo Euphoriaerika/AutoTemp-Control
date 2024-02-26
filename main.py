df6158c9ef306c41458ff482733d4b4f6b000e67
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-02-26 19:09:06 +0200

    fix: fixed bag whith ctrl.Rule

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def z1_fuction(x, a, b):
 
 
 # Визначення області значень x
-x1_values = np.linspace(-90, 90, 1000)
+x1_values = np.linspace(-90, 90, 181)
 
 # Обчислення значень функції z(x)
 z1_values = z1_fuction(x1_values, -72, -36)
@@ -31,7 +31,7 @@ def z2_function(x, a, b):
 
 
 # Визначення області значень x
-x2_values = np.linspace(-90, 90, 1000)
+x2_values = np.linspace(-90, 90, 181)
 
 # Обчислення значень функції z(x)
 z2_values = z2_function(x2_values, 36, 72)
@@ -49,13 +49,13 @@ plt.show()
 temperature = ctrl.Antecedent(np.arange(0, 101, 1), "temperature")
 
 # Визначення області значень х
-x3_values = np.linspace(0, 100, 1000)
+x3_values = np.linspace(0, 100, 101)
 
 # Визначення нечітких множин та їх функцій належності
 temperature["cold"] = z1_fuction(x3_values, 10, 30)
 temperature["cool"] = fuzz.trimf(temperature.universe, [20, 35, 50])
 temperature["medium"] = fuzz.trimf(temperature.universe, [40, 50, 60])
-temperature["not wery hot"] = fuzz.trimf(temperature.universe, [50, 60, 70])
+temperature["not very hot"] = fuzz.trimf(temperature.universe, [50, 60, 70])
 temperature["hot"] = z2_function(x3_values, 60, 70)
 
 # Визначення вихідної змінної
@@ -69,18 +69,18 @@ tap["slight angle to the right"] = fuzz.trimf(tap.universe, [0, 27, 54])
 tap["large angle to the right"] = z2_function(x2_values, 36, 72)
 
 # Визначення правил
-rule1 = ctrl.Rule(temperature, ["hot"], tap["large angle to the right"])
-rule2 = ctrl.Rule(temperature, ["not very hot"], tap["slight angle to the right"])
-rule3 = ctrl.Rule(temperature, ["medium"], tap["OK"])
-rule4 = ctrl.Rule(temperature, ["cool"], tap["slight angle to the left"])
-rule5 = ctrl.Rule(temperature, ["cold"], tap["large angle to the left"])
+rule1 = ctrl.Rule(temperature["hot"], tap["large angle to the right"])
+rule2 = ctrl.Rule(temperature["not very hot"], tap["slight angle to the right"])
+rule3 = ctrl.Rule(temperature["medium"], tap["OK"])
+rule4 = ctrl.Rule(temperature["cool"], tap["slight angle to the left"])
+rule5 = ctrl.Rule(temperature["cold"], tap["large angle to the left"])
 
 # Створення системи керування
 tap_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5])
 tap_system = ctrl.ControlSystemSimulation(tap_ctrl)
 
 # Встановлення значення температури (від 0 до 100)
-tap_system.input["temperature"] = 96
+tap_system.input["temperature"] = 50
 
 # Обчислення вихідного значення
 tap_system.compute()
