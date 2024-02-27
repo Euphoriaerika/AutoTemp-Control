2015c48b2ce4d92005adeecaf4da8a69f5bca653
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-02-27 19:41:17 +0200

    docs: added comments to my POST request, updated language of comments in all files

--- calculation_tap.py
+++ calculation_tap.py
@@ -3,93 +3,89 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
 
-# Визначення Z-функції
-def z1_fuction(x, a, b):
+# Definition of the left Z-function
+def z1_function(x, a, b):
     return np.where(x <= a, 1, np.where(x < b, 1 - (x - a) / (b - a), 0))
 
-# Визначення Z-функції
+# Definition of the right Z-function
 def z2_function(x, a, b):
     return np.where(x <= a, 0, np.where(x < b, (x - a) / (b - a), 1))
 
 def calculation_tap(input, show=False):
-    # Визначення області значень x
+    # Definition of x1 values range
     x1_values = np.linspace(-90, 90, 181)
 
-    # Обчислення значень функції z(x)
-    z1_values = z1_fuction(x1_values, -72, -36)
+    # Computation of z1 function values
+    z1_values = z1_function(x1_values, -72, -36)
 
     if show:
-        # Побудова графіку
+        # Plotting the graph of the left function
         plt.figure(figsize=(8, 5))
         plt.plot(x1_values, z1_values, "b", linewidth=1.5)
-        plt.title("Zeta1 Fuction")
+        plt.title("Zeta1 Function")
         plt.xlabel("x")
         plt.ylabel("z(x)")
         plt.grid(True)
         plt.show()
 
-
-    # Визначення області значень x
+    # Definition of x2 values range
     x2_values = np.linspace(-90, 90, 181)
 
-    # Обчислення значень функції z(x)
+    # Computation of z2 function values
     z2_values = z2_function(x2_values, 36, 72)
 
     if show:
-        # Побудова графіку
+        # Plotting the graph of the right function
         plt.figure(figsize=(8, 5))
         plt.plot(x2_values, z2_values, "b", linewidth=1.5)
-        plt.title("Zeta2 Fuction")
+        plt.title("Zeta2 Function")
         plt.xlabel("x")
         plt.ylabel("z(x)")
         plt.grid(True)
         plt.show()
 
-    # Визначення змінних вхідних
+    # Definition of input variables
     temperature = ctrl.Antecedent(np.arange(0, 101, 1), "temperature")
 
-    # Визначення області значень х
+    # Definition of x3 values range
     x3_values = np.linspace(0, 100, 101)
 
-    # Визначення нечітких множин та їх функцій належності
-    temperature["cold"] = z1_fuction(x3_values, 10, 30)
+    # Definition of fuzzy sets and their membership functions
+    temperature["cold"] = z1_function(x3_values, 10, 30)
     temperature["cool"] = fuzz.trimf(temperature.universe, [20, 35, 50])
     temperature["medium"] = fuzz.trimf(temperature.universe, [40, 50, 60])
     temperature["not very hot"] = fuzz.trimf(temperature.universe, [50, 60, 70])
     temperature["hot"] = z2_function(x3_values, 60, 70)
 
-    # Визначення вихідної змінної
+    # Definition of output variable
     tap = ctrl.Consequent(np.arange(-90, 91, 1), "tap")
 
-    # Визначення функції належності для крана
-    tap["large angle to the left"] = z1_fuction(x1_values, -72, -36)
+    # Definition of membership functions for the tap
+    tap["large angle to the left"] = z1_function(x1_values, -72, -36)
     tap["slight angle to the left"] = fuzz.trimf(tap.universe, [-54, -27, 0])
     tap["OK"] = fuzz.trimf(tap.universe, [-18, 0, 18])
     tap["slight angle to the right"] = fuzz.trimf(tap.universe, [0, 27, 54])
     tap["large angle to the right"] = z2_function(x2_values, 36, 72)
 
-    # Визначення правил
+    # Definition of rules
     rule1 = ctrl.Rule(temperature["hot"], tap["large angle to the right"])
     rule2 = ctrl.Rule(temperature["not very hot"], tap["slight angle to the right"])
     rule3 = ctrl.Rule(temperature["medium"], tap["OK"])
     rule4 = ctrl.Rule(temperature["cool"], tap["slight angle to the left"])
     rule5 = ctrl.Rule(temperature["cold"], tap["large angle to the left"])
 
-    # Створення системи керування
+    # Creation of the control system
     tap_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5])
     tap_system = ctrl.ControlSystemSimulation(tap_ctrl)
 
-    # Встановлення значення температури (від 0 до 100)
+    # Setting the temperature value (from 0 to 100)
     tap_system.input["temperature"] = input
 
-    # Обчислення вихідного значення
+    # Computing the output value
     tap_system.compute()
 
     if show:
-        # Виведення результату
+        # Displaying the result
         print("Water Tap:", tap_system.output["tap"])
 
     return tap_system.output["tap"]
-
-
-
--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-from calculation_tap import calculation_tap
-from flask import Flask, request, jsonify
-from flask_cors import CORS  # Додайте цей імпорт
+from calculation_tap import calculation_tap  # Importing the calculation_tap function from a module
+from flask import Flask, request, jsonify  # Importing necessary modules from Flask
+from flask_cors import CORS  # Adding CORS support for handling cross-origin requests
 
-app = Flask(__name__)
-CORS(app)  # Додайте це для вирішення проблеми CORS
+app = Flask(__name__)  # Creating a Flask application instance
+CORS(app)  # Allowing Cross-Origin Resource Sharing (CORS) for the Flask app
 
-@app.route('/update', methods=['POST'])
+@app.route('/update', methods=['POST'])  # Defining a route for updating water angle via POST request
 def update_water_angle():
     try:
-        temperature = request.json['temperature']
-        # Викликайте функцію для визначення кута відповідно до температури
-        angle = calculation_tap(temperature)
-        return jsonify({'angle': angle})
+        temperature = request.json['temperature']  # Extracting temperature data from the JSON request
+
+        angle = calculation_tap(temperature)  # Calculating the angle needed to achieve comfortable temperature
+        return jsonify({'angle': angle})  # Returning the calculated angle as JSON response
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({'error': str(e)})  # Returning an error message if an exception occurs
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=True)  # Running the Flask app in debug mode if executed as the main script
